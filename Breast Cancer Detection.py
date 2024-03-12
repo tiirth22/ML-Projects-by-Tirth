857cf29fc7baf21e9ccc05b61d9e50c4c349dbb2
tiirth22 <152633332+tiirth22@users.noreply.github.com>
2024-03-12 10:26:05 +0530

    Create Breast Cancer Detection.py
    This Python script constitutes a machine learning endeavor designed to aid in the early-stage diagnosis of breast cancer among individuals. On the project's inception, Day 1 involved the inclusion of essential libraries requisite for the program's functionality. This initial setup marks the foundational step toward implementing algorithms and methodologies aimed at accurately diagnosing and assisting patients afflicted with breast cancer.

---
+++ Breast Cancer Detection.py
@@ -0,0 +1,17 @@
+"""This GitHub code has been crafted by TIRTH JIGNESHKUMAR DALAL from CHARUSAT UNIVERSITY with a focus on simplifying and streamlining processes. 
+It offers a user-friendly approach to solving problems, ensuring accessibility and ease of use for all stakeholders. 
+The code demonstrates a commitment to clarity and efficiency, providing intuitive solutions to complex challenges. 
+With clear documentation and concise implementation, this code fosters collaboration and empowers users to navigate and contribute effectively. 
+It reflects a dedication to enhancing the developer experience and fostering a welcoming environment for innovation and growth."""
+
+# Import necessary libraries DAY 1 
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB                 
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score
+
+
+
+
